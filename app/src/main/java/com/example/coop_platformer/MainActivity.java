58ca34f55b02b7890c43e21a8a9b9450e356bed7
OMEGA-5656 <abhisheksaini642794@gmail.com>
2024-09-06 20:49:44 +0530

    change kotlin to java

---
+++ app/src/main/java/com/example/coop_platformer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.coop_platformer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
